9f8166627ecc7bee6bafd8e96dacce1d75ec9a38
Yulei <fengyulei2009@126.com>
2017-11-01 10:41:49 +0100

    delet folder

--- jQuery/twig/index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require_once "vendor/autoload.php";
-
-$loader = new Twig_Loader_Filesystem("templates");
-$twig   = new Twig_Environment($loader,array('cache' => false));
-
-echo $twig->render("twig-exemple.html.twig");
-
-?>
